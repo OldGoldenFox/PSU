f76b4cc425af616d01914afab42aa6b5b189684d
Danil <Luntik-04@mail.ru>
2026-02-17 19:33:16 +0500

    Init

---
+++ RTPO/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace RTPO
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class ExampleTests : PageTest
+    {
+        [Test]
+        public async Task MainPage_ShouldHaveTitle()
+        {
+            await Page.GotoAsync("https://playwright.dev");
+            
+            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+        }
+    }
+}
