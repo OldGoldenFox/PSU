5486dd0e3de4ab6ff18143d7a7962bc2bb047069
ayatova-alima <Ayatova.ak9@icloud.com>
2026-02-19 18:48:05 +0500

    Forms is done

---
+++ PlaywrightDemoQA/Pages/Forms/PracticeFormPage.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightDemoQA.Pages.Forms;
+
+public class PracticeFormPage
+{
+    private readonly IPage _page;
+    
+    public ILocator FirstNameInput => _page.Locator("#firstName");
+    public ILocator LastNameInput => _page.Locator("#lastName");
+    public ILocator EmailInput => _page.Locator("#userEmail");
+    public ILocator MobileInput => _page.Locator("#userNumber");
+    public ILocator CurrentAddressInput => _page.Locator("#currentAddress");
+    
+    public ILocator GenderMale => _page.GetByText("Male", new() { Exact = true });
+    
+    public ILocator HobbySports => _page.GetByText("Sports");
+
+    public ILocator DateOfBirthInput => _page.Locator("#dateOfBirthInput");
+
+    public ILocator SubjectsInput => _page.Locator("#subjectsInput");
+
+    public ILocator UploadPictureInput => _page.Locator("#uploadPicture");
+
+    public ILocator StateDropdown => _page.Locator("#state");
+    public ILocator CityDropdown => _page.Locator("#city");
+    
+    // Ну вот эту штуку я вообще нз как было сделать, ее с геминьки нагло взял
+    public ILocator StateCityInput => _page.Locator("input[id^='react-select']");
+
+    public ILocator SubmitButton => _page.Locator("#submit");
+    
+    public ILocator SuccessModal => _page.Locator(".modal-content");
+    public ILocator ModalTable => _page.Locator(".table-responsive");
+    
+    public PracticeFormPage(IPage page) => _page = page;
+    
+    public async Task Open()
+    {
+        await _page.GotoAsync("https://demoqa.com/");
+        await _page.ClickAsync("text=Forms");
+
+        var radioMenuItem = _page.Locator("span.text:text-is('Practice Form')");
+        await radioMenuItem.ScrollIntoViewIfNeededAsync();
+        await radioMenuItem.ClickAsync(new() { Force = true });
+
+        await _page.Locator("h1").WaitForAsync();
+    }
+}
---
+++ PlaywrightDemoQA/Tests/Forms/PracticeFormTests.cs
@@ -0,0 +1,57 @@
+﻿using PlaywrightDemoQA.Pages.Forms;
+
+namespace PlaywrightDemoQA.Tests.Forms
+{
+    public class PracticeFormTests: BaseTest
+    {
+        private PracticeFormPage _practiceFormPage;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            _practiceFormPage = new PracticeFormPage(Page);
+            await _practiceFormPage.Open();
+        }
+        
+        [Test]
+        public async Task FillFullForm()
+        {
+            await _practiceFormPage.FirstNameInput.FillAsync("Danil");
+            await _practiceFormPage.LastNameInput.FillAsync("Pavlovich");
+            await _practiceFormPage.EmailInput.FillAsync("dan@mail.com");
+            await _practiceFormPage.GenderMale.ClickAsync();
+            await _practiceFormPage.MobileInput.FillAsync("87072573615");
+
+            // Вот этот блок тоже думал как сделать, че-то пытался через выбор даты, а потом узнал что можно проще
+            await _practiceFormPage.DateOfBirthInput.ClickAsync();
+            await Page.Keyboard.PressAsync("Control+A");
+            await Page.Keyboard.TypeAsync("06 Jun 2004");
+            await Page.Keyboard.PressAsync("Enter");
+            
+            await _practiceFormPage.SubjectsInput.FillAsync("Maths");
+            await Page.Keyboard.PressAsync("Enter");
+            
+            await _practiceFormPage.HobbySports.ClickAsync();
+            
+            var photoPath = Path.Combine(Directory.GetCurrentDirectory(), "test_photo.jpg");
+            await File.WriteAllTextAsync(photoPath, "Ты снова смотришь -_-");
+            await _practiceFormPage.UploadPictureInput.SetInputFilesAsync(photoPath);
+            
+            await _practiceFormPage.CurrentAddressInput.FillAsync("Улица Пушкина, дом Колотушкина");
+            
+            await _practiceFormPage.StateDropdown.ClickAsync();
+            await Page.Keyboard.TypeAsync("NCR");
+            await Page.Keyboard.PressAsync("Enter");
+            
+            await _practiceFormPage.CityDropdown.ClickAsync();
+            await Page.Keyboard.TypeAsync("Delhi");
+            await Page.Keyboard.PressAsync("Enter");
+            
+            await _practiceFormPage.SubmitButton.ClickAsync();
+            
+            await Expect(_practiceFormPage.SuccessModal).ToBeVisibleAsync();
+            await Expect(_practiceFormPage.ModalTable).ToContainTextAsync("Danil Pavlovich");
+            await Expect(_practiceFormPage.ModalTable).ToContainTextAsync("dan@mail.com");
+        }
+    }
+}
