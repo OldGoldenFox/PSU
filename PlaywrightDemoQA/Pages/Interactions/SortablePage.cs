32208eb09bc4743cb1c2e20b85a921d0278d3ce0
OldGoldenFox <1danilvyrodov1@gmail.com>
2026-02-20 12:42:41 +0500

    Interactions 3/5 is done

---
+++ PlaywrightDemoQA/Pages/Interactions/SortablePage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightDemoQA.Pages.Interactions
+{
+    public class SortablePage
+    {
+        private readonly IPage _page;
+
+        public ILocator ListItems => _page.Locator(".vertical-list-container .list-group-item");
+
+        public SortablePage(IPage page) => _page = page;
+        
+        public async Task Open()
+        {
+            await _page.GotoAsync("https://demoqa.com/", new() { WaitUntil = WaitUntilState.DOMContentLoaded });
+            await _page.ClickAsync("text=Interactions");
+        
+            var menuItem = _page.Locator("span.text:text-is('Sortable')");
+            await menuItem.ScrollIntoViewIfNeededAsync();
+            await menuItem.ClickAsync(new() { Force = true });
+        
+            await _page.Locator("h1").WaitForAsync();
+        }
+    }
+}
---
+++ PlaywrightDemoQA/Tests/Interactions/SortableTests.cs
@@ -0,0 +1,27 @@
+﻿using PlaywrightDemoQA.Pages.Interactions;
+
+namespace PlaywrightDemoQA.Tests.Interactions
+{
+    public class SortableTests: BaseTest
+    {
+        private SortablePage _sortablePage;
+
+        [SetUp]
+        public new async Task Setup()
+        {
+            _sortablePage = new SortablePage(Page);
+            await _sortablePage.Open();
+        }
+        
+        [Test]
+        public async Task Sortable_MoveItem_Test()
+        {
+            var itemOne = _sortablePage.ListItems.GetByText("One");
+            var itemFour = _sortablePage.ListItems.GetByText("Four");
+
+            await itemOne.DragToAsync(itemFour, new() { TargetPosition = new() { X = 10, Y = 40 } });
+            
+            await Expect(_sortablePage.ListItems.Nth(3)).ToHaveTextAsync("One");
+        }
+    }
+}
